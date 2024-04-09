9d3fadb8ebab0e777a2c8177f5cdc1ea0daaaf83
Kiryu <13matheusilva@gmail.com>
2024-04-09 16:32:30 -0300

    header style add

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,27 +1,31 @@
 import React from "react";
 import logo from '../../images/logo.svg'
 
+import './header.css'
+
 const Header = () => {
     return (
         <header className="header">
-            <a href="/">
-                <img src={logo} alt="logo" className="logo"></img>
-            </a>
+            <div className="header-content">
+                <a href="/">
+                    <img src={logo} alt="logo" className="logo"></img>
+                </a>
 
-            <nav>
-                <div className="mobile-menu">
-                    <div className="line1"></div>
-                    <div className="line2"></div>
-                    <div className="line3"></div>
-                </div>
+                <nav>
+                    <div className="mobile-menu">
+                        <div className="line1"></div>
+                        <div className="line2"></div>
+                        <div className="line3"></div>
+                    </div>
 
-                <ul className="nav-list">
-                    <li className="nav-link"><a href="#about">About</a></li>
-                    <li className="nav-link"><a href="#services">Services</a></li>
-                    <li className="nav-link"><a href="#projects">Projects</a></li>
-                    <li className="nav-link"><a href="#contact">Contact</a></li>
-                </ul>
-            </nav>
+                    <ul className="nav-list">
+                        <li className="nav-link"><a href="#about">About</a></li>
+                        <li className="nav-link"><a href="#services">Services</a></li>
+                        <li className="nav-link"><a href="#projects">Projects</a></li>
+                        <li className="nav-link"><a href="#contact">Contact</a></li>
+                    </ul>
+                </nav>
+            </div>
         </header>
     )
 }
