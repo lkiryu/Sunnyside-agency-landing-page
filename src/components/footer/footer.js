eb79b3a4c2199d92ced1e2012352dd21886442c2
Kiryu <13matheusilva@gmail.com>
2024-04-09 16:02:50 -0300

    footer component add

---
+++ src/components/footer/footer.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <footer className="footer" id="contact">
+            <h3>sunnyside</h3>
+
+
+            <ul className="links">
+                <li><a href="#about">About</a></li>
+                <li><a href="#services">Services</a></li>
+                <li><a href="#projects">Projects</a></li>
+            </ul>
+
+            <ul className="social">
+                <li><a href="/"><i className="fa-brands fa-square-facebook"></i></a></li>
+                <li><a href="/"><i className="fa-brands fa-instagram"></i></a></li>
+                <li><a href="/"><i className="fa-brands fa-twitter"></i></a></li>
+                <li><a href="/"><i className="fa-brands fa-pinterest"></i></a></li>
+            </ul>
+        </footer>
+    )
+}
+
+export default Footer
