7676c76c246da929b9b3e01afabd92cf214ced2b
Kiryu <13matheusilva@gmail.com>
2024-04-09 17:10:59 -0300

    about section style add

--- src/components/aboutSection/aboutSection.js
+++ src/components/aboutSection/aboutSection.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+import './aboutSection.css'
+
 const About = () => {
     return(
         <section className="about" id="about">
@@ -7,7 +9,7 @@ const About = () => {
                 <h3>Transform your brand</h3>
                 <p>We are a full-service creative agency specializing in helping brands grow fast.
                     Engage your clients through compelling visuals that do most of the marketing for you.</p>
-                <a href="/" className="btn-learn-more-right">Learn more</a>
+                <a href="/" className="btn-learn-more-left">Learn more</a>
             </div>
             <div className="image right"></div>
             <div className="image left"></div>
@@ -15,7 +17,7 @@ const About = () => {
                 <h3>Stand out to the right audience</h3>
                 <p>Using a collaborative formula of designers, researchers, photographers, videographers, and
                     copywriters, we’ll build and extend your brand in digital places.</p>
-                <a href="/" className="btn-learn-more-left">Learn more</a>
+                <a href="/" className="btn-learn-more-right">Learn more</a>
             </div>
         </section>
     )
