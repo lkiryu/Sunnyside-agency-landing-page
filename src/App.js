021f9e1395dac099ae37cfe8d6d407e2dc1f984c
Kiryu <13matheusilva@gmail.com>
2024-04-09 15:11:17 -0300

    header component add

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Header from './components/header/header';
 
 function App() {
   return (
     <main className='container'>
-      
+      <Header />
     </main>
   );
 }
