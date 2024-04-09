823cdbc6868a3257188b4b738e803e7e92368eee
Kiryu <13matheusilva@gmail.com>
2024-04-09 16:12:04 -0300

    landing page style add

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import './App.css';
 import Header from './components/header/header';
 import Hero from './components/hero/hero'
 import About from './components/aboutSection/aboutSection';
@@ -6,6 +5,8 @@ import Services from './components/servicesSection/servicesSection';
 import Testimonials from './components/testimonials/testimonials';
 import Projects from './components/projectsSections/projectsSections';
 import Footer from './components/footer/footer';
+
+import './App.css';
 
 function App() {
   return (
