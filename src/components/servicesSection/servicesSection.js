8d26b49f4cfb7845ce9d815a9a8770060b212be0
kiryu <13matheusilva@gmail.com>
2024-05-22 14:30:02 -0300

    services map add

--- src/components/servicesSection/servicesSection.js
+++ src/components/servicesSection/servicesSection.js
@@ -2,19 +2,32 @@ import React from "react";
 
 import './servicesSection.css'
 
+const servicesData = [
+    {
+        title: "Graphic design",
+        description: "Great design makes you memorable. We deliver artwork that underscores your brand message and captures potential clients’ attention.",
+        side: "left",
+    },
+    {
+        title: "Photography",
+        description: "Increase your credibility by getting the most stunning, high-quality photos that improve your business image.",
+        side: "right",
+    },
+]
+
 const Services = () => {
-    return(
+    return (
         <section className="services" id="services">
-            <div className="service right">
-                <h3>Graphic design</h3>
-                <p>Great design makes you memorable. We deliver artwork that underscores your brand message and captures
-                    potential clients’ attention.</p>
-            </div>
-            <div className="service left">
-                <h3>Photography</h3>
-                <p>Increase your credibility by getting the most stunning, high-quality photos that improve your
-                    business image.</p>
-            </div>
+            {
+                servicesData.map((item, index) => {
+                    return (
+                        <div key={index} className={`service ${item.side}`}>
+                            <h3>{item.title}</h3>
+                            <p>{item.description}</p>
+                        </div>
+                    )
+                })
+            }
         </section>
     )
 }
